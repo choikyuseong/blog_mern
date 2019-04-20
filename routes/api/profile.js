3ab164b1a05c6e21dbd9331935534961a3d4e243
choikyuseong <kusung2230@naver.com>
2019-04-20 11:57:07 +0900

    current profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,84 @@
+// const express = require('express');
+// const router = express.Router();
+// const mongoose = require('mongoose');
+// const passport = require('passport');
+// const authcheck = passport.authenticate('jwt', {session : false});
+//
+// const profileModel = require('../../models/profile');
+// const userModel = require('../../models/user');
+//
+//
+//
+//
+// // @router Get api/profile/test
+// // @desc Tests profile route
+// // @access Public
+// router.get('/test' , (req,res) => res.json({msg:'profile'}));
+//
+//
+//
+// // @router Get api/profile
+// // @desc get current users profile
+// // @access private
+// router.get('/', authcheck, (req, res) => {
+//     const errors={};
+//     profileModel.findOne({ user: req.user.id })
+//         .then(profile => {
+//             if (!profile){
+//                 errors.noprofile = 'There is no profile for this user';
+//                 return res.status(404).json(errors);
+//             }
+//             res.json(profile);
+//
+//
+//
+//         })
+//         .catch(err => res.json(err));
+// });
+//
+// module.exports = router;
+
+
+
+
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+
+const profileModel = require('../../models/Profile');
+const userModel = require('../../models/User');
+
+const authCheck = passport.authenticate('jwt', { session: false });
 
 
 
-// @router Get api/users/profile
-// @desc Tests users route
-// @access Public
-router.get('/test' , (req,res) => res.json({msg:'profile'}));
+// @route   GET api/profile/test
+// @desc    Tests users route
+// @access  Public
+router.get('/test', (req, res) => res.json({ msg: 'Profile Works' }));
+
+
+// @route   GET api/profile
+// @desc    Get current users profile
+// @access  Private
+router.get('/', authCheck, (req, res) => {
+
+    const errors = {};
+
+    profileModel.findOne({ user: req.user.id })
+        .then(profile => {
+            if (!profile) {
+                errors.noprofile = 'There is no profile for this user';
+                return res.status(404).json(errors);
+            }
+            res.json(profile);
+        })
+        .catch(err => res.json(err));
+
+});
+
+
+
 module.exports = router;
