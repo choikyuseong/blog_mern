ee1d1ce59868a5fd74ba16cbf5e7959958567f82
choikyuseong <kusung2230@naver.com>
2019-05-01 12:44:44 +0900

    add React client

--- server.js
+++ server.js
@@ -52,5 +52,5 @@ app.use('/api/posts' , postsRoutes);
 
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Sever running on port ${PORT}`));
