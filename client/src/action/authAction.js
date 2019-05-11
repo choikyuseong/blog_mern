aa89b24d2150968c11dd62105ceba70530aae91a
choikyuseong <kusung2230@naver.com>
2019-05-11 11:27:29 +0900

    redux login action and set user

--- client/src/action/authAction.js
+++ client/src/action/authAction.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS , SET_CURRENT_USER } from "./types";
+import jwt_decode from 'jwt-decode';
+import setAuthToken from '../utils/setAuthToken';
 
 
-//Register User
+//Register User  test
 // export const registerUser = userData => {
 //     return {
 //         type: TEST_DISPATCH,
@@ -11,6 +13,8 @@ import { GET_ERRORS } from "./types";
 // };
 
 
+
+//Register User
 export const registerUser = (userData, history) => dispatch => {
     axios
         .post('/api/users/register', userData)
@@ -25,3 +29,42 @@ export const registerUser = (userData, history) => dispatch => {
 };
 
 
+//Login - Get User Token
+export const loginUser = userDate => dispatch => {
+
+    axios
+        .post('/api/users/login',userDate)
+        .then(res => {
+            //save to localstorage
+            const { token } = res.data;
+
+            //set token to ls
+            localStorage.setItem('jwtToken', token);
+
+            //set token to auth header
+            setAuthToken(token);
+
+            //decode token to get user data
+            const decoded = jwt_decode(token);
+
+            //set current user
+            dispatch(setCurrentUser(decoded));
+        })
+        .catch(err =>
+            dispatch({
+                type:GET_ERRORS,
+                payload:err.response.data
+            })
+        );
+
+
+};
+
+
+//Set logged in user
+export const setCurrentUser = decoded =>{
+    return{
+        type: SET_CURRENT_USER,
+        payload: decoded
+    };
+};
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,5 +1,6 @@
 // import { TEST_DISPATCH} from "../action/types";
-
+import isEmpty from '../validation/is-empty';
+import { SET_CURRENT_USER} from "../action/types";
 
 const initialState = {
     isAuthenticated: false,
@@ -9,7 +10,12 @@ const initialState = {
 
 export default function(state = initialState, action) {
     switch (action.type) {   //switch case
-
+        case SET_CURRENT_USER:
+            return {
+                ...state,
+                isAuthenticated: !isEmpty(action.payload),
+                user: action.payload
+            };
         // case TEST_DISPATCH:
         //     return {
         //         ...state,
