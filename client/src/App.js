d8e9fa8f341d11d6b053fabab8659a1a086e3cc6
choikyuseong <kusung2230@naver.com>
2019-05-11 10:19:07 +0900

    app.js provider store add

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,42 @@
+import React , {Component} from 'react';
+import { BrowserRouter as Router , Route } from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from './store';
 
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 
-import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 
 import './App.css';
 
+
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Navbar />
-                <Route exact path="/" component={Landing} />
-                <div className="container">
-                    <Route exact path="/register" component={Register} />
-                    <Route exact path="/login" component={Login} />
-                </div>
-                <Footer />
+  return (
+
+      <Provider store={store}>
+      <Router>
+        <div className="App">
+            <Navbar/>
+            <Route exact path="/" component={Landing} />
+
+            <div className="container">
+                <Route exact path="/Register" component={Register} />
+                <Route exact path="/Login" component={Login} />
             </div>
-        </Router>
-    );
+
+            <Footer/>
+
+
+
+        </div>
+      </Router>
+      </Provider>
+  );
 }
 
 export default App;
